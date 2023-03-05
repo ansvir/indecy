b08b7a3abf3eed6d249f31571d654a605aa25315
tohant <svirepa.anton@gmail.com>
2023-03-05 11:34:46 +0100

    Add physical dependencies to project (builds)

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dfile=indecy-1.0.1-SNAPSHOT.jar -DgroupId=org.itique.indecy -DartifactId=indecy -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=indecy-groovy-1.0.1-SNAPSHOT.jar -DgroupId=org.itique.indecy -DartifactId=indecy-groovy -Dversion=1.0.1 -Dpackaging=jar
+mvn install:install-file -Dfile=indecy-java-1.0.1-SNAPSHOT.jar -DgroupId=org.itique.indecy -DartifactId=indecy-java -Dversion=1.0.1 -Dpackaging=jar
